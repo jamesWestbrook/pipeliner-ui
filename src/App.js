8ff9377484b014261f188ac8049405bba1f887ef
james westbrook <james.m.westbrook@gmail.com>
2019-08-27 07:29:22 -0400

    adding landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Landing'
+import Landing from './Landing';
 
 function App() {
-  return (
+  return (    
     <div className="App">
+      <Landing></Landing>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Landing extends React.Component {
+    
+    async componentWillMount() {
+    }
+
+    render() {
+        return (<div></div>);
+    }
+}
